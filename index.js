5bcc0b92429cbfe1b63049424ecb51e7f1cb70c2
Vlad Voiculescu <vldvoiculescu@gmail.com>
2025-01-26 05:31:17 +0200

    final

---
+++ index.js
@@ -0,0 +1,19 @@
+let randomNumber1 = Math.round(Math.random() * 5) + 1;
+let randomNumber2 = Math.round(Math.random() * 5) + 1;
+
+document.querySelector(".img1").setAttribute("src", "./images/dice" + randomNumber1 + ".png");
+document.querySelector(".img2").setAttribute("src", "./images/dice" + randomNumber2 + ".png");
+
+if(randomNumber1 > randomNumber2) {
+
+  document.querySelector("h1").textContent = "Player 1 Wins!";
+
+} else if (randomNumber1 < randomNumber2) {
+
+  document.querySelector("h1").textContent = "Player 2 Wins!";
+
+} else {
+
+  document.querySelector("h1").textContent = "Draw!";
+
+}
